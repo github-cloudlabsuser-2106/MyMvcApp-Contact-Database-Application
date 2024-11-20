a8c72dceedf128adfa0ca650789437f2203df0be
github-cloudlabsuser-2106 <github_cloudlabsuser_2106@cloudlabsaiuser.com>
2024-11-20 17:23:12 +0000

    challenge 2

--- Models/User.cs
+++ Models/User.cs
@@ -2,15 +2,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace MyMvcApp.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
 
     }
 }
